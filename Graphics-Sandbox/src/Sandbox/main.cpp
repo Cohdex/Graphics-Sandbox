d4df094c32cf6376b28d99d4a3b52390efba88a7
Jonathan Cohlin <jonathan.cohlin@gmail.com>
2019-05-22 22:35:27 +0200

    added solution

---
+++ Graphics-Sandbox/src/Sandbox/main.cpp
@@ -0,0 +1,7 @@
+#include <pch.h>
+
+int main() {
+	std::cout << "Hello world!" << std::endl;
+
+	return 0;
+}
---
+++ premake5.lua
@@ -0,0 +1,77 @@
+workspace "Graphics-Sandbox"
+	architecture "x86_64"
+	configurations { "Debug", "Develop", "Release" }
+	startproject "Graphics-Sandbox"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Graphics-Sandbox"
+	location "Graphics-Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "Graphics-Sandbox/src/pch.cpp"
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{prj.name}/src/",
+		"%{prj.name}/extlib/GLFW/include/",
+		"%{prj.name}/extlib/GLM/include/"
+	}
+
+	libdirs {
+		"%{prj.name}/extlib/GLFW/"
+	}
+
+	links {
+		"glad",
+		"OpenGL32",
+		"glfw3"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter { "configurations:Debug or Develop" }
+		includedirs {
+			"%{prj.name}/extlib/Glad/Glad-debug/include/"
+		}
+		libdirs {
+			"%{prj.name}/extlib/Glad/Glad-debug/"
+		}
+
+	filter { "configurations:Release" }
+		includedirs {
+			"%{prj.name}/extlib/Glad/Glad/include/"
+		}
+		libdirs {
+			"%{prj.name}/extlib/Glad/Glad/"
+		}
+
+	filter "configurations:Debug"
+		defines "DMN_DEBUG"
+		symbols "On"
+		optimize "Off"
+		runtime "Debug"
+		
+	filter "configurations:Develop"
+		defines "DMN_DEVELOP"
+		symbols "Off"
+		optimize "On"
+		runtime "Release"
+
+	filter "configurations:Release"
+		defines "DMN_RELEASE"
+		symbols "Off"
+		optimize "Full"
+		runtime "Release"
